b6f5c6063a2151f8f658b66d0ab3f77c7dac11f8
globus <ten.work400@gmail.com>
2024-11-13 18:05:13 +0300

    исправил ошибку с AppCompatImageButton

--- app/src/main/java/com/example/pageforregister/MainPageActivity.kt
+++ app/src/main/java/com/example/pageforregister/MainPageActivity.kt
@@ -3,9 +3,7 @@ package com.example.pageforregister
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
