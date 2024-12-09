f79decd331f81a1eb3091d3d352f157ff3719e0c
globus <ten.work400@gmail.com>
2024-12-09 16:06:51 +0300

    Добавил таймер подключение к серверу, подсоеденил кнопку Back в card

--- app/src/main/java/com/example/pageforregister/networkapi/RetrofitInstance.kt
+++ app/src/main/java/com/example/pageforregister/networkapi/RetrofitInstance.kt
@@ -5,9 +5,10 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://45.80.70.37:5000/" // Убедитесь, что заменили на ваш IP
+    private const val BASE_URL = "http://45.80.70.37:5000" // Убедитесь, что заменили на ваш IP
 
     private val logging = HttpLoggingInterceptor().setLevel(
         if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
@@ -16,6 +17,9 @@ object RetrofitInstance {
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(logging)
+        .connectTimeout(120, TimeUnit.SECONDS) // Установка таймаута на подключение
+        .readTimeout(120, TimeUnit.SECONDS)    // Установка таймаута на чтение
+        .writeTimeout(120, TimeUnit.SECONDS)   // Установка таймаута на запись
         .build()
 
     val api: ApiService by lazy {
@@ -26,4 +30,5 @@ object RetrofitInstance {
             .build()
             .create(ApiService::class.java)
     }
+
 }
